fc9235019995399821b90214fe87f82902945822
Dierik28 <diego.alfonso@uabc.edu.mx>
2025-05-11 23:35:06 -0700

    Se agregaron nuevos metodos en la clase CartaEquipo para tener la herencia de la clase Carta

--- CartaEquipo.java
+++ CartaEquipo.java
@@ -1,18 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class CartaEquipo extends Carta {
     private String nombreEquipo;
+    private String nombreLiga;
+    private boolean esLiga;
 
-    public CartaEquipo(String nombreEquipo) {
+    public CartaEquipo(String nombreEquipo, String nombreLiga, boolean esLiga) {
         super();
         this.nombreEquipo = nombreEquipo;
+        this.nombreLiga = nombreLiga;
+        this.esLiga = esLiga;
     }
 
     public String getNombreEquipo() {
         return nombreEquipo;
     }
 
+    public String getNombreLiga() {
+        return nombreLiga;
+    }
+
+    public boolean esLiga() {
+        return esLiga;
+    }
+
     @Override
     public boolean esIgualA(Carta carta) {
         return carta instanceof CartaEquipo otraCarta &&
                 this.nombreEquipo.equals(otraCarta.nombreEquipo);
+    }
+
+    @Override
+    public ImageIcon getImagen() {
+        String nombreImagen;
+        if(esLiga){
+            nombreImagen = nombreLiga + ".png";
+        }else{
+            nombreImagen = nombreEquipo + ".png";
+        }
+        String ruta = "C:\\Users\\14321\\Downloads\\Practica-7\\cartasFutbolImagenes\\" + nombreImagen;
+        // String ruta = "C:\\Users\\PC OSTRICH\\Practica-7\\cartasFutbolImagenes" + nombreImagen;
+        ImageIcon icono = new ImageIcon(ruta);
+        Image imagenEscalada = icono.getImage().getScaledInstance(115, 200, Image.SCALE_SMOOTH);
+        return new ImageIcon(imagenEscalada);
     }
 }
