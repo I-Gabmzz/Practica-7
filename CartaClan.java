f2b41733634eb4b3c9551bceb81795d9588cde96
I-Gabmzz <a1197503@uabc.edu.mx>
2025-05-12 09:37:21 -0700

    Comentarios CartaClan.java
    Se le asignan los comentarios de CartaCLan

--- CartaClan.java
+++ CartaClan.java
@@ -1,11 +1,15 @@
+// Se importan las librerias necesarias.
 import javax.swing.*;
 import java.awt.*;
 
+// Se define la creacion de la clase CartaClan la cual se extiende de la clase Carta.
 public class CartaClan extends Carta {
+    // Se definen los atributos pertenecientes a la clase.
     private String clan;
     private String personaje;
     private boolean esClan;
 
+    // Metodo constructor de la clase el cual crea una carta en base a una clan, un personaje y un identificador acerca si este es personaje o clan.
     public CartaClan(String clan, String personaje, boolean esClan) {
         super();
         this.clan = clan;
@@ -13,24 +17,29 @@ public class CartaClan extends Carta {
         this.esClan = esClan;
     }
 
+    // Metodo getter el cual brinda el clan que tiene asignado la carta.
     public String getClan() {
         return clan;
     }
 
+    // Metodo getter el cual brinda el personaje que tiene asignado la carta.
     public String getPersonaje() {
         return personaje;
     }
 
+    // Metodo booleano el cual devuelve si la carta es una clan o es un personaje.
     public boolean esClan() {
         return esClan;
     }
 
+    // Metodo derivado de la herencia de la clase Carta, este metodo tiene la finalidad de comparar una carta con otra.
     @Override
     public boolean esIgualA(Carta carta) {
         return carta instanceof CartaClan otraCarta &&
                 this.clan.equals(otraCarta.clan);
     }
 
+    // Metodo derivado de la herencia de la clase Carta, este metodo tiene la finalidad de asignarle la imagen correspondiente a la carta. 
     @Override
     public ImageIcon getImagen() {
         String nombreImagen;
