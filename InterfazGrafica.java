47eca365beba79b578760d2e12a7acb8f18c4fd8
Dierik28 <diego.alfonso@uabc.edu.mx>
2025-05-11 20:52:13 -0700

    Se agregaron metodos en la clase InterfazGrafica para poder ver los banners de los modos de juego y las cartas ocultas en el mismo.

--- InterfazGrafica.java
+++ InterfazGrafica.java
@@ -31,7 +31,8 @@ public class InterfazGrafica {
         panelDeTitulo.add(labelBienvenida);
 
         JPanel panelCentro = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        ImageIcon imagenIcono = new ImageIcon("C:\\Users\\PC OSTRICH\\Practica-7\\pantallaInicial.gif");
+        //ImageIcon imagenIcono = new ImageIcon("C:\\Users\\PC OSTRICH\\Practica-7\\pantallaInicial.gif");
+        ImageIcon imagenIcono = new ImageIcon("C:\\Users\\14321\\IdeaProjects\\Practica-7\\pantallaInicial.gif");
         JLabel labelImagen = new JLabel(imagenIcono);
         panelCentro.add(labelImagen);
 
@@ -274,15 +275,18 @@ public class InterfazGrafica {
         JPanel panelFutbolMatch = new JPanel(new BorderLayout(5, 5));
         JPanel panelGeoMatch = new JPanel(new BorderLayout(5, 5));
 
-        ImageIcon imagenNaruto = new ImageIcon("C:\\Users\\PC OSTRICH\\Practica-7\\cartasNarutoImagenes\\menuN.png");
+        //ImageIcon imagenNaruto = new ImageIcon("C:\\Users\\PC OSTRICH\\Practica-7\\cartasNarutoImagenes\\menuN.png");
+        ImageIcon imagenNaruto = new ImageIcon("C:\\Users\\14321\\IdeaProjects\\Practica-7\\cartasNarutoImagenes\\menuN.png");
         Image imagenEscaladaN = imagenNaruto.getImage().getScaledInstance(350, 550, Image.SCALE_SMOOTH);
         imagenNaruto = new ImageIcon(imagenEscaladaN);
 
-        ImageIcon imagenFutbolMatch = new ImageIcon("C:\\Users\\PC OSTRICH\\Practica-7\\cartasFutbolImagenes\\menuFutbol.png");
+        //ImageIcon imagenFutbolMatch = new ImageIcon("C:\\Users\\PC OSTRICH\\Practica-7\\cartasFutbolImagenes\\menuFutbol.png");
+        ImageIcon imagenFutbolMatch = new ImageIcon("C:\\Users\\14321\\IdeaProjects\\Practica-7\\cartasFutbolImagenes\\menuFutbol.png");
         Image imagenEscaladaF = imagenFutbolMatch.getImage().getScaledInstance(350, 550, Image.SCALE_SMOOTH);
         imagenFutbolMatch = new ImageIcon(imagenEscaladaF);
 
-        ImageIcon imagenGeoMatch = new ImageIcon("C:\\Users\\PC OSTRICH\\Practica-7\\cartasGeoMatchImagenes\\menuGeo.png");
+        //ImageIcon imagenGeoMatch = new ImageIcon("C:\\Users\\PC OSTRICH\\Practica-7\\cartasGeoMatchImagenes\\menuGeo.png");
+        ImageIcon imagenGeoMatch = new ImageIcon("C:\\Users\\14321\\IdeaProjects\\Practica-7\\cartasGeoMatchImagenes\\menuGeo.png");
         Image imagenEscaladaG = imagenGeoMatch.getImage().getScaledInstance(350, 550, Image.SCALE_SMOOTH);
         imagenGeoMatch = new ImageIcon(imagenEscaladaG);
 
@@ -364,6 +368,40 @@ public class InterfazGrafica {
             case 1 -> " \uD83C\uDF00 Naruto ";
             case 2 -> " \u26BD FutbolMatch ";
             case 3 -> " \uD83C\uDF0D GeoMatch ";
+            default -> "";
+        };
+    }
+
+    public static String getBannerPrincipal(int modo) {
+        return switch (modo) {
+            case 1 -> " \uD83C\uDF00 Modo Naruto ";
+            //case 2 -> "C:\\Users\\PC OSTRICH\\Practica-7\\bannerFutbolMatch.png";
+            case 2 -> "C:\\Users\\14321\\IdeaProjects\\Practica-7\\bannerFutbolMatch.png";
+            //case 3 -> "C:\\Users\\PC OSTRICH\\Practica-7\\bannerGeoMatch.png";
+            case 3 -> "C:\\Users\\14321\\IdeaProjects\\Practica-7\\bannerGeoMatch.png";
+            default -> "";
+        };
+    }
+
+    public static String getBannerCentral(int modo) {
+        return switch (modo) {
+            case 1 -> " \uD83C\uDF00 Modo Naruto ";
+            //case 2 -> "C:\\Users\\PC OSTRICH\\Practica-7\\futbolMatchCentral.gif";
+            case 2 -> "C:\\Users\\14321\\IdeaProjects\\Practica-7\\futbolMatchCentral.gif";
+            //case 3 -> "C:\\Users\\PC OSTRICH\\Practica-7\\GeoMatchCentral.gif";
+            case 3 -> "C:\\Users\\14321\\IdeaProjects\\Practica-7\\GeoMatchCentral.gif";
+            default -> "";
+        };
+    }
+
+    public static String getCartasOcultas(int modo) {
+        return switch (modo) {
+            //case 1 -> "C:\\Users\\PC OSTRICH\\Practica-7\\cartasNarutoImagenes\\fondoNaruto.png";
+            case 1 -> "C:\\Users\\14321\\IdeaProjects\\Practica-7\\cartasNarutoImagenes\\fondoNaruto.png";
+            //case 2 -> "C:\\Users\\PC OSTRICH\\Practica-7\\cartasFutbolImagenes\\fondoFut.png";
+            case 2 -> "C:\\Users\\14321\\IdeaProjects\\Practica-7\\cartasFutbolImagenes\\fondoFut.png";
+            //case 3 -> "C:\\Users\\PC OSTRICH\\Practica-7\\cartasGeoMatchImagenes\\PaisGeo.png";
+            case 3 -> "C:\\Users\\14321\\IdeaProjects\\Practica-7\\cartasGeoMatchImagenes\\PaisGeo.png";
             default -> "";
         };
     }
